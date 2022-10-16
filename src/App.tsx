5ff0eeefc6a2fe75a525721849e82158fb704f37
caioamisterdam <caioamisterdam@gmail.com>
2022-10-16 11:07:35 -0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './styles/global.css'
+
+export function App() {
+  return (
+    <>
+    <h1 className="font-bold text-2xl text-violet-500">Hello World</h1>
+
+    <button className="bg-cyan-500 text-md px-4 py-2 rounded text-black hover:bg-cyan-300">
+    Enviar
+    </button>
+    </>
+    )
+}
