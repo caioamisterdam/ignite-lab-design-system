0c47061ac7975e6d9b8b018ce09d14724aea97d5
caioamisterdam <caioamisterdam@gmail.com>
2022-10-17 00:40:49 -0300

    after

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,9 @@
+import { SignIn } from './pages/SignIn';
+
 import './styles/global.css'
 
 export function App() {
   return (
-    <>
-    <h1 className="font-bold text-2xl text-violet-500">Hello World</h1>
-
-    <button className="bg-cyan-500 text-md px-4 py-2 rounded text-black hover:bg-cyan-300">
-    Enviar
-    </button>
-    </>
+    <SignIn />
     )
 }
